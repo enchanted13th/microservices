1f93020cb3def04bca0e6d5db7c20dedf3a8a829
ine <enchanted13th@gmail.com>
2020-12-24 10:13:01 +0900

    add product-composite-service test case

--- microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -3,6 +3,7 @@ package se.magnus.microservices.core.review.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.review.Review;
 import se.magnus.api.core.review.ReviewService;
 import se.magnus.util.exceptions.InvalidInputException;
@@ -11,6 +12,7 @@ import se.magnus.util.http.ServiceUtil;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class ReviewServiceImpl implements ReviewService {
     private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
 
