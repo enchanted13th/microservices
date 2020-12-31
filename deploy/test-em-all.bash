9c2d6854cbf80e861e6853ab42a64bfcb9680c50
ine <enchanted13th@gmail.com>
2020-12-31 14:05:44 +0900

    modify test-em-all.bash

--- deploy/test-em-all.bash
+++ deploy/test-em-all.bash
@@ -40,10 +40,53 @@ function assertEqual() {
   fi
 }
 
+function testUrl() {
+  url=$@
+  if curl $url -ks -f -o /dev/null
+  then
+    echo "OK"
+    return 0
+  else
+    echo -n "not yet"
+    return 1
+  fi;
+}
+
+function waitForService() {
+  url=$@
+  echo -n "Wait for: $url... "
+  n=0
+  until testUrl $url
+  do
+    n=$((n + 1))
+    if [[ $n == 100 ]]
+    then
+      echo " Give up"
+      exit 1
+    else
+      sleep 6
+      echo -n ", retry #$n "
+    fi
+  done
+}
+
 set -e
+
+echo "Start:" `date`
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
+
+if [[ $@ == *"start"* ]]
+then
+  echo "Restarting the test environment..."
+  echo "$ docker-compose down"
+  docker-compose down
+  echo "$ docker-compose up -d"
+  docker-compose up -d
+fi
+
+waitForService http://$HOST:$PORT/product-composite/1
 
 # Verify that a normal request works
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/1 -s"
@@ -74,6 +117,11 @@ assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
 assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
 
+if [[ $@ == *"stop"* ]]
+then
+  echo "We are done, stopping the test environment..."
+  echo "$ docker-compose down"
+  docker-compose down
+fi
 
-
-
+echo "End:" `date`
