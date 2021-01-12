f3a6ea6d648ff5bfda1606e0a98f4f44aa71bdd0
ine <enchanted13th@gmail.com>
2021-01-12 22:42:57 +0900

    refactor review-service

--- api/src/main/java/se/magnus/api/core/review/ReviewService.java
+++ api/src/main/java/se/magnus/api/core/review/ReviewService.java
@@ -1,26 +1,13 @@
 package se.magnus.api.core.review;
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public interface ReviewService {
 
-    /**
-     * Sample usage:
-     *
-     * curl -X POST $HOST:$PORT/review \
-     *  -H "Content-Type: application/json" --data \
-     *  '{"productId":123,"reviewId":456,"author":"me","subject":"yada, yada, yada","content":"yada, yada, yada"}'
-     *
-     * @param body
-     * @return
-     */
-    @PostMapping(
-            value    = "/review",
-            consumes = "application/json",
-            produces = "application/json"
-    )
+
     Review createReview(@RequestBody Review body);
 
     /**
@@ -32,18 +19,8 @@ public interface ReviewService {
     @GetMapping(
         value    = "/review",
         produces = "application/json")
-    List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+    Flux<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
 
-    /**
-     * Sample usage:
-     *
-     * curl -X DELETE $HOST:$PORT/review?productId=1
-     *
-     * @param productId
-     */
-    @DeleteMapping(
-            value = "/review"
-    )
     void deleteReviews(@RequestParam(value = "productId", required = true) int productId);
 
 }
